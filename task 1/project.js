7c5abab5e9b3e7b8e149147f35017b0e3aefe519
Maxim <shidloba@gmail.com>
2023-01-29 02:44:27 +0300

    create_project

---
+++ task 1/project.js
@@ -0,0 +1,19 @@
+const rnd = Math.round(Math.random(100) * 100)
+console.log(rnd);
+
+let start = 0;
+let end = 100;
+let middle = Math.round((start + end) / 2)
+
+for (let i = star; i < end; i++) {
+    if (middle === rnd) {
+        console.log(rnd, `вы угадали`);
+        break
+    } else if (middle > rnd) {
+        end = middle
+        console.log(`число меньше ${rnd}`);
+    } else if (middle < rnd) {
+        start = middle
+        console.log(`число больше ${rnd}`);
+    }
+}
